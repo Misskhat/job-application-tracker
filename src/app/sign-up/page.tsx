95d63ae77c9bf01c5205ecb6a8272c2b836c9156
Md. Misskhat Hossen Milon <misskhat.hossen.com>
2026-06-14 21:11:06 +0600

    signup page and database connected

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useForm, SubmitHandler } from "react-hook-form";
 import Link from "next/link";
 import { Button } from "../../components/ui/button";
 import {
@@ -12,8 +12,52 @@ import {
 } from "../../components/ui/card";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
+import { useState } from "react";
+import { signUp } from "../../lib/auth/auth-client";
+import { email } from "better-auth";
+import { useRouter } from "next/navigation";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleOnSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data);
+    setLoading(true);
+    const name = data.name;
+    const email = data.email;
+    const password = data.password;
+    try {
+      const result = await signUp.email({
+        name,
+        email,
+        password,
+      });
+
+      if (result.error) {
+        console.log(result.error.message ?? "Failed to signUp");
+      } else {
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center">
       <Card className="w-full max-w-md border-gray-200 shadow-lg">
@@ -25,7 +69,7 @@ const SignUp = () => {
             Create an account to start tracking your job applications.
           </CardDescription>
         </CardHeader>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit(handleOnSubmit)} className="space-y-4">
           <CardContent className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="name" className="text-gray-700">
@@ -36,8 +80,11 @@ const SignUp = () => {
                 type="text"
                 placeholder="John Doe"
                 className="border-gray-300 focus:border-primary focus:ring-primary"
-                required
+                {...register("name", { required: true })}
               />
+              {errors.name && (
+                <span className="text-red-500 italic">Name is required</span>
+              )}
             </div>
             <div className="space-y-2">
               <Label htmlFor="email" className="text-gray-700">
@@ -48,8 +95,11 @@ const SignUp = () => {
                 type="email"
                 placeholder="johndoe@email.com"
                 className="border-gray-300 focus:border-primary focus:ring-primary"
-                required
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <span className="text-red-500 italic">Email is required</span>
+              )}
             </div>
             <div className="space-y-2">
               <Label htmlFor="password" className="text-gray-700">
@@ -59,18 +109,28 @@ const SignUp = () => {
                 id="password"
                 type="password"
                 placeholder="****"
-                minLength={8}
                 className="border-gray-300 focus:border-primary focus:ring-primary"
-                required
+                {...register("password", { required: true, minLength: 8 })}
               />
+              {errors.password?.type === "required" && (
+                <span className="text-red-500 italic">
+                  Password is required
+                </span>
+              )}
+              {errors.password?.type === "minLength" && (
+                <span className="text-red-500 italic">
+                  Password must be 8 digit.
+                </span>
+              )}
             </div>
           </CardContent>
           <CardFooter className="flex flex-col space-y-4">
             <Button
               type="submit"
               className="w-full bg-primary hover:bg-primary/90"
+              disabled={loading}
             >
-              Sign Up
+              {loading ? "Try to signup...." : "Sign Up"}
             </Button>
             <p>
               Already have an account? Please{" "}
